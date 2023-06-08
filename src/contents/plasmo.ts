04e3bbff131082140cfc2cc78076c2ebbc0a3945
yoshiishunichi <syun_syun01@icloud.com>
2023-06-08 20:48:01 +0900

    :sparkles: ツイートの追加を監視

--- src/contents/plasmo.ts
+++ src/contents/plasmo.ts
@@ -4,11 +4,30 @@ export const config: PlasmoCSConfig = {
   matches: ["https://tweetdeck.twitter.com/"],
 };
 
+const mutatinObserverConfig: MutationObserverInit = {
+  childList: true,
+};
+
+const callback = (mutations: MutationRecord[]) => {
+  for (const mutation of mutations) {
+    if (mutation.type === "childList" && mutation.removedNodes.length === 0) {
+      console.log("ツイート追加されたぞ");
+    }
+  }
+};
+const observer = new MutationObserver(callback);
+
 const readStart = () => {
   button.onclick = readEnd;
   button.textContent = "読み上げ終了";
 
   // タイムライン監視の処理を書く
+  const tweetContainerList = document.querySelectorAll(".js-chirp-container");
+  const tweetContainer = tweetContainerList[0];
+
+  if (tweetContainer) {
+    observer.observe(tweetContainer, mutatinObserverConfig);
+  }
 };
 
 const readEnd = () => {
@@ -16,6 +35,7 @@ const readEnd = () => {
   button.textContent = "読み上げ開始";
 
   // タイムライン監視の終了の処理を書く};
+  observer.disconnect();
 };
 
 const button = document.createElement("button");
